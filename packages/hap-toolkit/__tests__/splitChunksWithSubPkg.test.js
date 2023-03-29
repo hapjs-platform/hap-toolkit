ebcd1320ab1dd50d4b6e8b353dfb3d740ff202fa
pengjie <pengjie.rjyb@vivo.com>
2023-03-29 17:31:41 +0800

    test: update test
    Signed-off-by: pengjie <pengjie.rjyb@vivo.com>

--- packages/hap-toolkit/__tests__/splitChunksWithSubPkg.test.js
+++ packages/hap-toolkit/__tests__/splitChunksWithSubPkg.test.js
@@ -25,57 +25,57 @@ describe('split chunks on a project', () => {
 
   // 公共js结合分包兼容卡片
   it('split chunks with subpackages', async () => {
-    // const mode = 'dev'
-    // // 独立拥有完整的共用js内容
-    // // 非独立包只拥有该分包引用的共用js内容
-    // const { stats } = await compile(platform, mode, false, {
-    //   cwd: projectRoot,
-    //   splitChunksMode: 'SMART'
-    // })
-    // expect(stats.hasErrors()).toBe(false)
-    // const result = glob.sync('**/*.json', {
-    //   cwd: buildDir
-    // })
-    // expect(result).toMatchSnapshot()
-    // // 测试分包内容
-    // const pkgs = [
-    //   { name: 'com.application.demo.rpk', jsonPath: 'app-chunks.json', pageChunks: '' },
-    //   { name: 'com.application.demo.rpk', jsonPath: 'page-chunks.json', pageChunks: '' },
-    //   { name: 'com.application.demo.base.srpk', jsonPath: 'page-chunks.json', pageChunks: '' },
-    //   { name: 'com.application.demo.base.srpk', jsonPath: 'app-chunks.json', pageChunks: '' },
-    //   {
-    //     name: 'com.application.demo.sub2.srpk',
-    //     jsonPath: 'Sub2/page-chunks.json',
-    //     pageChunks: ''
-    //   },
-    //   { name: 'com.application.demo.sub3.srpk', jsonPath: 'Sub3/page-chunks.json', pageChunks: '' },
-    //   { name: 'com.application.demo.Card.srpk', jsonPath: 'Card/page-chunks.json', pageChunks: '' }
-    // ]
-    // const rpks = path.join(projectRoot, 'dist/com.application.demo.debug.1.0.0.rpks')
-    // const rpksBuf = fs.readFileSync(rpks)
-    // const rpksZipBuf = await JSZip.loadAsync(rpksBuf)
-    // for (let i = 0; i < pkgs.length; i++) {
-    //   const pkg = pkgs[i]
-    //   // 获取 rpks 里面的各个 rpk/srpk 的 buffer
-    //   const pkgBuf = rpksZipBuf.file(pkg.name).async('nodebuffer')
-    //   // 获取 rpk/srpk 里的各项文件 zip buffer
-    //   const pkgZipBuf = await JSZip.loadAsync(pkgBuf)
-    //   const pageChunks = pkgZipBuf.file(pkg.jsonPath)
-    //   if (pkg.jsonPath === 'Card/page-chunks.json') {
-    //     // 卡片端内无page-chunks.json
-    //     expect(!pageChunks).toBeTruthy()
-    //   } else {
-    //     // 应用端内每个包都有page-chunks.json
-    //     expect(!!pageChunks).toBeTruthy()
-    //     let pageChunksJson = await pageChunks.async('string')
-    //     // 仅记录key值即可
-    //     pageChunksJson = JSON.stringify(JSON.parse(pageChunksJson), (key, val) => {
-    //       return key ? '' : val
-    //     })
-    //     pkg.pageChunks = pageChunksJson.replace(/\\/gm, '/')
-    //   }
-    // }
-    // expect(pkgs).toMatchSnapshot()
+    const mode = 'dev'
+    // 独立拥有完整的共用js内容
+    // 非独立包只拥有该分包引用的共用js内容
+    const { stats } = await compile(platform, mode, false, {
+      cwd: projectRoot,
+      splitChunksMode: 'SMART'
+    })
+    expect(stats.hasErrors()).toBe(false)
+    const result = glob.sync('**/*.json', {
+      cwd: buildDir
+    })
+    expect(result).toMatchSnapshot()
+    // 测试分包内容
+    const pkgs = [
+      { name: 'com.application.demo.rpk', jsonPath: 'app-chunks.json', pageChunks: '' },
+      { name: 'com.application.demo.rpk', jsonPath: 'page-chunks.json', pageChunks: '' },
+      { name: 'com.application.demo.base.srpk', jsonPath: 'page-chunks.json', pageChunks: '' },
+      { name: 'com.application.demo.base.srpk', jsonPath: 'app-chunks.json', pageChunks: '' },
+      {
+        name: 'com.application.demo.sub2.srpk',
+        jsonPath: 'Sub2/page-chunks.json',
+        pageChunks: ''
+      },
+      { name: 'com.application.demo.sub3.srpk', jsonPath: 'Sub3/page-chunks.json', pageChunks: '' },
+      { name: 'com.application.demo.Card.srpk', jsonPath: 'Card/page-chunks.json', pageChunks: '' }
+    ]
+    const rpks = path.join(projectRoot, 'dist/com.application.demo.debug.1.0.0.rpks')
+    const rpksBuf = fs.readFileSync(rpks)
+    const rpksZipBuf = await JSZip.loadAsync(rpksBuf)
+    for (let i = 0; i < pkgs.length; i++) {
+      const pkg = pkgs[i]
+      // 获取 rpks 里面的各个 rpk/srpk 的 buffer
+      const pkgBuf = rpksZipBuf.file(pkg.name).async('nodebuffer')
+      // 获取 rpk/srpk 里的各项文件 zip buffer
+      const pkgZipBuf = await JSZip.loadAsync(pkgBuf)
+      const pageChunks = pkgZipBuf.file(pkg.jsonPath)
+      if (pkg.jsonPath === 'Card/page-chunks.json') {
+        // 卡片端内无page-chunks.json
+        expect(!pageChunks).toBeTruthy()
+      } else {
+        // 应用端内每个包都有page-chunks.json
+        expect(!!pageChunks).toBeTruthy()
+        let pageChunksJson = await pageChunks.async('string')
+        // 仅记录key值即可
+        pageChunksJson = JSON.stringify(JSON.parse(pageChunksJson), (key, val) => {
+          return key ? '' : val
+        })
+        pkg.pageChunks = pageChunksJson.replace(/\\/gm, '/')
+      }
+    }
+    expect(pkgs).toMatchSnapshot()
   }, 50000)
 
   afterAll(async () => {
