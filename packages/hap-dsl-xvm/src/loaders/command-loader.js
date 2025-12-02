76d313aa386c3515a6b653f9e3630d8c5fed4c5f
unknown <W9025400@adc.com>
2025-12-02 19:45:15 +0800

    pretier格式修正

--- packages/hap-dsl-xvm/src/loaders/command-loader.js
+++ packages/hap-dsl-xvm/src/loaders/command-loader.js
@@ -1,4 +1,4 @@
- /* Copyright (c) 2024-present, the hapjs-platform Project Contributors
+/* Copyright (c) 2024-present, the hapjs-platform Project Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
 export default function commandLoader(source) {
