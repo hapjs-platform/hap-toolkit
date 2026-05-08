66f640c544d4e82239127b55aa44bf979a6bd226
xujunjie1 <xujunjie1@xiaomi.com>
2026-05-08 16:11:38 +0800

    progress type增加arc类型

--- packages/hap-compiler/src/template/validator.js
+++ packages/hap-compiler/src/template/validator.js
@@ -550,7 +550,7 @@ const tagNatives = {
         def: 0
       },
       type: {
-        enum: ['horizontal', 'circular', 'round', 'segmented']
+        enum: ['horizontal', 'circular', 'round', 'segmented', 'arc']
       }
     }
   },
