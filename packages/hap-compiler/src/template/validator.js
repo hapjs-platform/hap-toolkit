7c8a9035876d933f322625b909149d356367559f
lilei946 <snowinter2008@gmail.com>
2026-06-15 16:17:10 +0800

    feat: 增加公控组件的支持
    Signed-off-by: lilei946 <snowinter2008@gmail.com>

--- packages/hap-compiler/src/template/validator.js
+++ packages/hap-compiler/src/template/validator.js
@@ -217,7 +217,15 @@ const tagNatives = {
   },
   web: {
     atomic: true,
-    events: ['pagestart', 'pagefinish', 'titlereceive', 'error', 'message', 'progress', 'intercepturl'],
+    events: [
+      'pagestart',
+      'pagefinish',
+      'titlereceive',
+      'error',
+      'message',
+      'progress',
+      'intercepturl'
+    ],
     attrs: {
       src: {},
       trustedurl: {},
@@ -233,7 +241,7 @@ const tagNatives = {
       supportzoom: {
         enum: ['true', 'false']
       },
-      intercepturl:{}
+      intercepturl: {}
     }
   },
   list: {
@@ -353,6 +361,20 @@ const tagNatives = {
     supportCard: true,
     textContent: true,
     atomic: true
+  },
+  vbutton: {
+    supportCard: true,
+    textContent: true,
+    atomic: true,
+    attrs: {
+      enablecolor: {},
+      enableanim: {},
+      animtype: {},
+      loading: {},
+      drawtype: {},
+      text: {},
+      disable: {}
+    }
   },
   refresh: {
     attrs: {
@@ -577,6 +599,68 @@ const tagNatives = {
     },
     events: ['change', 'columnchange', 'cancel']
   },
+  vcustompicker: {
+    supportCard: true,
+    selfClosing: true,
+    atomic: true,
+    empty: true,
+    attrs: {
+      mode: {},
+      defaultvalue: {},
+      itemcycle: {},
+      visibleitemcount: {}
+    },
+    events: ['change']
+  },
+  vtimepicker: {
+    supportCard: true,
+    selfClosing: true,
+    atomic: true,
+    empty: true,
+    attrs: {
+      defaultvalue: {},
+      itemcycle: {},
+      visibleitemcount: {}
+    },
+    events: ['change']
+  },
+  vdatepicker: {
+    supportCard: true,
+    selfClosing: true,
+    atomic: true,
+    empty: true,
+    attrs: {
+      defaultvalue: {},
+      itemcycle: {},
+      yearrange: {},
+      visibleitemcount: {}
+    },
+    events: ['change']
+  },
+  vdatepicker2: {
+    supportCard: true,
+    selfClosing: true,
+    atomic: true,
+    empty: true,
+    attrs: {
+      defaultvalue: {},
+      itemcycle: {},
+      yearrange: {},
+      visibleitemcount: {}
+    },
+    events: ['change']
+  },
+  vcalendardatepicker: {
+    supportCard: true,
+    selfClosing: true,
+    atomic: true,
+    empty: true,
+    attrs: {
+      defaultvalue: {},
+      yearrange: {}
+    },
+    events: ['change']
+  },
   switch: {
     supportCard: true,
     selfClosing: true,
@@ -585,6 +669,50 @@ const tagNatives = {
       checked: {
         enum: ['false', 'true']
       }
+    },
+    events: ['change']
+  },
+  vswitch: {
+    supportCard: true,
+    selfClosing: true,
+    atomic: true,
+    empty: true,
+    attrs: {
+      checked: {},
+      blurmaterial: {},
+      disable: {},
+      loading: {}
+    },
+    events: ['change']
+  },
+  vcheckbox: {
+    supportCard: true,
+    selfClosing: true,
+    atomic: true,
+    empty: true,
+    attrs: {
+      text: {},
+      checked: {},
+      groupid: {},
+      type: {},
+      tickcolor: {},
+      framecolor: {},
+      fillcolor: {},
+      checkstatus: {}
+    },
+    events: ['change']
+  },
+  vradiobutton: {
+    supportCard: true,
+    selfClosing: true,
+    atomic: true,
+    empty: true,
+    attrs: {
+      text: {},
+      checked: {},
+      groupid: {},
+      radiocolor: {},
+      framecolor: {}
     },
     events: ['change']
   },
@@ -1328,7 +1456,7 @@ function checkTagName(node, output, options = {}) {
           log.push({
             line: location.line || 1,
             column: location.col || 1,
-            reason: 'WARN: 组件 `' + tagName + '` 不支持属性 `' + item
+            reason: 'WARN: 组件 `' + tagName + '` 不支持属性 `' + item + '`'
           })
         }
       }
